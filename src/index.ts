941a6c1ab2608f1782dbebd2cd522f2b8bec4e13
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2022-11-23 08:17:15 -0300

    first commit, setting up project

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response, Express } from 'express'
+const app = express()
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import cors from 'cors'
+
+
+
+dotenv.config()
+
+const PORT = process.env.PORT
+
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+app.get('/',
+(req: Request, res: Response) => {
+
+
+  res.send('connected!')
+})
+
+app.listen(PORT, () =>
+  console.log(`--\n--\n\nServer running at http://localhost:${PORT}\n\n--\n--`),
+)
